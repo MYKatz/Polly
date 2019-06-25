233079c8c319e075d6229e91d6187aad550ff3a4
Matt Katz <mykatz01@gmail.com>
2019-06-24 23:16:03 -0700

    Get config vars

--- main.go
+++ main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	//"github.com/bwmarrin/discordgo"
+	"github.com/spf13/viper"
+)
+
+var (
+	commandPrefix string
+	botID string
+	botKey string
 )
 
 func main() {
-	fmt.Println("And so it begins!")
+	commandPrefix, botKey := getConfigVars()
+	fmt.Printf(commandPrefix)
+	fmt.Printf(botKey)
+}
+
+func getConfigVars() (string, string) {
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error, check config file/environment variables: %s \n", err))
+	}
+	prefix := viper.GetString("COMMAND_PREFIX")
+	if prefix == "" || len(prefix) != 1 {
+		panic(fmt.Errorf("Fatal error, check COMMAND_PREFIX environment variable"))
+	}
+	key := viper.GetString("BOT_KEY")
+	if key == "" {
+		panic(fmt.Errorf("Fatal error, check BOT_KEY environment variable"))
+	}
+	return prefix, key
 }
