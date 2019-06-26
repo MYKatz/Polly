07b68995d316ffa753e3321351b6c7a2ebc50d83
Matt Katz <mykatz01@gmail.com>
2019-06-25 17:13:13 -0700

    Markdown formatting, begun on admin detection

--- commands.go
+++ commands.go
@@ -9,6 +9,24 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+func markdownWrapper(lang string, message string) string {
+	//hack for working with backticks in go strings
+	backticks := "`" + "`" + "`"
+	return backticks + lang + "\n" + message + backticks
+}
+
+func isAdmin(discord *discordgo.Session, userID string, channelID string) bool {
+	c, err := discord.Channel(channelID)
+	if err != nil {
+		return false
+	}
+	g, err := discord.Guild(c.GuildID)
+	if err != nil {
+		return false
+	}
+	//finish this later - look at DogBot on github for example
+}
+
 func commandChooser(discord *discordgo.Session, message *discordgo.MessageCreate) {
 	command := strings.Fields(strings.ToLower(message.Content)) //note that channel names (hashtags) get converted to ID numbers, so this doesn't affect them
 	command = append(command, "")
@@ -18,7 +36,12 @@ func commandChooser(discord *discordgo.Session, message *discordgo.MessageCreate
 	}
 	switch command[1] {
 	case "":
-		discord.ChannelMessageSend(message.ChannelID, "Help message")
+		msg := `= Welcome To Polly! =
+
+[ Commands ]
+	- setup #channel1 #channel2 #channel3... :: takes a space-separated list of channels to learn from. If channels are unspecified, will use all of them.
+	- dance :: a test command`
+		discord.ChannelMessageSend(message.ChannelID, markdownWrapper("asciidoc", msg))
 	case "setup":
 		discord.ChannelMessageSend(message.ChannelID, "Setup!")
 	case "dance":
